c13de0975fd0148e1e26ba15bcdc0791ad26bc42
Indrajit Sen <sen.indrajit92@gmail.com>
2021-09-17 02:07:36 +0530

    Enabling Cross Origin functionality.

--- src/main/java/com/mazdausa/mfpsalesms/MfpSaleMsApplication.java
+++ src/main/java/com/mazdausa/mfpsalesms/MfpSaleMsApplication.java
@@ -2,7 +2,10 @@ package com.mazdausa.mfpsalesms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @PropertySource("classpath:application.properties")
@@ -10,6 +13,16 @@ public class MfpSaleMsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MfpSaleMsApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 
 }
